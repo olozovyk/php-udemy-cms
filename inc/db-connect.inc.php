8ee7f26f1060aa392883602a1d9edec90ab34537
Oleh Lozovyk <oleh.lozovyk@gmail.com>
2025-10-28 14:07:05 +0000

    move static files to public and set .htaccess rules

--- inc/db-connect.inc.php
+++ inc/db-connect.inc.php
@@ -3,9 +3,10 @@
 $DB_USER = $_ENV['DB_USER'];
 $DB_PASSWORD = $_ENV['DB_PASSWORD'];
 $DB_NAME = $_ENV['DB_NAME'];
+$DB_HOST = $_ENV['DB_HOST'];
 
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=$DB_NAME;charset=utf8mb4", $DB_USER, $DB_PASSWORD, [
+    $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4", $DB_USER, $DB_PASSWORD, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 }
